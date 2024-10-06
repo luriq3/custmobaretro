158eb04b4853a706000c4b18370d4da290ba0928
luriq3 <fflhmm1@gmail.com>
2024-10-06 20:45:34 -0300

    Update custmoba.lua

--- custmoba.lua
+++ custmoba.lua
@@ -437,28 +437,7 @@ for i,v in ipairs (getSpectators(posz())) do
 end
 end)
 
-if type(storage.manatrainer) ~= "table" then
-  storage.manatrainer = {on=false, title="mana%", text="Power Down", min=0, max=90}
-end
 
-for _, healingInfos in ipairs({storage.manatrainer}) do
-  local healingmacro = macro(20, function()
-    local mana = manapercent()
-    if healingInfos.max <= mana and mana >= healingInfos.min then
-      if TargetBot then 
-        TargetBot.saySpell(healingInfos.text)
-      else
-        say(healingInfos.text)
-      end
-    end
-  end)
-  healingmacro.setOn(healingInfos.on)
-
-  UI.DualScrollPanel(healingInfos, function(widget, newParams) 
-    healingInfos = newParams
-    healingmacro.setOn(healingInfos.on)
-  end)
-end 
 
 
 local castBelowHp = 70 
